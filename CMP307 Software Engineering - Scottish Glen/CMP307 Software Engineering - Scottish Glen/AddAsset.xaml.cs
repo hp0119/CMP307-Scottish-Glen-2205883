0f7d74b04394c808ba981c0fbb298f56db1910f3
hp0119 <2205883@uad.ac.uk>
2022-11-10 00:09:45 +0000

    Develop the Adding Asset Window into the Software Solution

--- CMP307 Software Engineering - Scottish Glen/CMP307 Software Engineering - Scottish Glen/AddAsset.xaml.cs
+++ CMP307 Software Engineering - Scottish Glen/CMP307 Software Engineering - Scottish Glen/AddAsset.xaml.cs
@@ -10,29 +10,31 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CMP307_Software_Engineering___Scottish_Glen
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddAsset.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddAsset : Window
     {
-        public MainWindow()
+        public AddAsset()
         {
             InitializeComponent();
         }
 
-        private void AddAssetButton(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // opens the add new item window
-            AddAsset AddA = new AddAsset();
-            AddA.Show();
+
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Clicked(object sender, RoutedEventArgs e)
         {
 
         }
